8fbdb22d71f41960bf5aa511fac85b504063442b
Sean Bordelon <sean.j.bordelon@gmail.com>
2020-05-27 05:16:22 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  
+  word = array.pop
+  
+  if (array.size() == 0)
+    array << word
+    array.join()
+  elsif (array.size() == 1)
+    array << " and #{word}"
+    array.join()
+  else
+    array << "and #{word}"
+    array.join(", ")
+  end
+  
 end
